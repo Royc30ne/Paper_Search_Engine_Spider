4fe0cab274ab276719956d0e2412e6320b417f47
Royc30ne <79168138+Royc30ne@users.noreply.github.com>
2022-02-02 21:43:50 +0000

    Update requirements.txt

--- run.py
+++ run.py
@@ -2,5 +2,6 @@ import IEEE_Xplore_spider as ixs
 
 if __name__ == '__main__':
     # Enter the keywords that need to be searched here
-    ieee = ixs.XploreSpider(r'("online risk*” OR "online abuse" OR "mental health") AND "deep learning"')
+    search_title = r'("online risk*” OR "online abuse" OR "mental health") AND "deep learning"'
+    ieee = ixs.XploreSpider(search_title)
     ieee.operate()
