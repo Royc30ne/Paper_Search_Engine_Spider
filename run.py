5f5903a06f3c07123a552e19d56d5ce095560658
Royc30ne <79168138+Royc30ne@users.noreply.github.com>
2022-02-03 08:26:59 +0000

    Update Command-Line Interface

--- run.py
+++ run.py
@@ -14,20 +14,20 @@ if __name__ == '__main__':
         print("2. ACM Digit Library (Under Construction)")
         print("3. PubMed            (Under Construction)")
         print("4. Exit")
-        search_engine = input("Pls type pure number: ")
+        search_engine = input("Pls type pure number: \n")
         if (search_engine == '4'):
             SystemExit()
 
         elif (search_engine == '1'):
             while (True):
-                content = input("Please type your your search content: ")
+                content = input("Please type your your search content: \n")
                 print("You want to search: " + content + ' , Right?')
-                ieee_confirm = input('Enter [y] to Confirm or back to Previous Menu: ')
+                ieee_confirm = input('Enter [y] to Confirm or back to Previous Menu: \n')
                 if (ieee_confirm == "Y" or ieee_confirm == "y"):
                     ieee = ixs.XploreSpider(content)
                     ieee.operate()
                     ieee_loop = input(
-                        "Do you want to continue searching? Enter [y] to continue or back to Previous Menu: ")
+                        "Do you want to continue searching? Enter [y] to continue or back to Previous Menu: \n")
                     if (ieee_loop != 'y'):
                         break
                 else:
